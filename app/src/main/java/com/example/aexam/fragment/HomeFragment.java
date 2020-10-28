06a371393747634d25e05a06923e2ecb4bc9621c
Liang <3495268188@qq.com>
2020-10-28 15:47:48 +0800

    主页初步

--- app/src/main/java/com/example/aexam/fragment/HomeFragment.java
+++ app/src/main/java/com/example/aexam/fragment/HomeFragment.java
@@ -4,18 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.aexam.R;
+import com.example.aexam.adapter.HomeGridAdapter;
 
 public class HomeFragment  extends Fragment {
-
+    private GridView gridView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        gridView = view.findViewById(R.id.gv_fragment_home);
+        HomeGridAdapter adapter = new HomeGridAdapter();
+        gridView.setAdapter(adapter);
     }
 }
